442ab1ec39f3dbaf317d65f4e8fd26b7914def91
anttilan <antti.lankinen@helsinki.fi>
2025-12-09 13:36:59 +0200

    Pelin muokkaukseen esto, kun pelaajia liittynyt

--- routes/admin.js
+++ routes/admin.js
@@ -54,7 +54,21 @@ exports.admin = (router) => {
 
     router.put('/game/edit', async (req, res) => {
         const { body } = req;
+
+        const gameId = body?.game_id;
+        if (!gameId) {
+            return res.status(400).json({ error: 'gameId is required in request body' });
+        }
+
         try {
+            const playerCount = await dbClient(`/api/game/playercount/${gameId}`);
+            if (
+                (Array.isArray(playerCount) && playerCount.length > 0) ||
+                (!Array.isArray(playerCount) && Number(playerCount) > 0)
+            ) {
+                return res.status(400).json({ error: 'edit_game_form_error_notification' });
+            }
+
             const response = await dbClient('/api/game/edit', {
                 method: 'put',
                 body: JSON.stringify(body),
@@ -163,7 +177,6 @@ exports.admin = (router) => {
     router.get('/games/:gameId/summary', async (req, res) => {
         const { gameId } = req.params;
         const eppn = req.user.eppn;
-        await dbApi.getAdminGameSummary({ ...req, params: { gameId, eppn }}, res);
+        await dbApi.getAdminGameSummary({ ...req, params: { gameId, eppn } }, res);
     });
-
 };
